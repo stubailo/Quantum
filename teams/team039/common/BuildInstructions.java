d8ec608db2b185d0f905be2dcdb0bff889f3ab00
Sashko <sashko@mit.edu>
2011-01-05 14:25:53 -0800

    added buildinstructions.java

---
+++ teams/team039/common/BuildInstructions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package team039.common;
+
+/**
+ *
+ * @author sashko
+ */
+public class BuildInstructions {
+
+    private int totalCost = 0;
+    private int numSteps = 0;
+
+   // private BuildStep[] steps;
+
+    public BuildInstructions ( String[] names, Object[] components )
+    {
+
+    }
+
+
+}
