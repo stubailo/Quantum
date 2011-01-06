3aa3a4b0018446a6aa6874c6e72de0917a0eb029
jrshoch <jrshoch@gmail.com>
2011-01-06 18:24:12 -0500

    Added State

---
+++ teams/team039/common/RobotState.java
@@ -0,0 +1,11 @@
+package team039.common;
+
+public enum RobotState {
+    // Just listed the first ones that came to mind.
+    STARTING,
+    EXPLORING,
+    BUILDING,
+    FLEEING,
+    ATTACKING,
+    DEFENDING
+}
