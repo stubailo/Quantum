d174e56e5acf8b293561be8471131c2c49564127
Antony Speranza <dreadtoad@antony-speranzas-macbook-2.local>
2011-01-07 01:56:39 -0500

    .....

--- teams/team039/common/QuantumConstants.java
+++ teams/team039/common/QuantumConstants.java
@@ -10,7 +10,7 @@ public final class QuantumConstants {
 	/** Total flux needed to explore for more mines */
 	public static final int TOTAL_FLUX_EXPLORE = 120;
 	/** Number of locations to remember for bug navigation */
-	public static final int BUG_MEMORY_LENGTH = (GameConstants.MAP_MAX_HEIGHT * GameConstants.MAP_MAX_WIDTH)/4;
+	public static final int BUG_MEMORY_LENGTH = 15;
 	/** Bug distance increase before trying different path */
 	public static final double BUG_DISTANCE_SWITCH = 1.2;
 	
