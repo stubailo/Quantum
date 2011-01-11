ac1233bf295fe3dacc2e20c39f410c0e2d4c8795
Antony <asperanz@mit.edu>
2011-01-10 19:47:39 -0500

    asdf

--- teams/team039/common/QuantumConstants.java
+++ teams/team039/common/QuantumConstants.java
@@ -24,7 +24,7 @@ public final class QuantumConstants {
 	/** Number of rounds to navigate before setting a new exploration goal */
 	public static final int EXPLORE_TIME = 100;
 	/** Distance to exploration goal */
-	public static final int EXLPORE_GOAL_DISTANCE = 30;
+	public static final int EXLPORE_GOAL_DISTANCE = 15;
 	
 	
 	/** Planck's constant */
