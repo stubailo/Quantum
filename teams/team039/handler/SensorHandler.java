6ebe41b777309e8b392ab951231511842f8241cc
jrshoch <jrshoch@gmail.com>
2011-01-10 20:08:03 -0500

    starting handler re-organization

---
+++ teams/team039/handler/SensorHandler.java
@@ -0,0 +1,23 @@
+package team039.handler;
+
+import battlecode.common.*;
+import team039.common.Knowledge;
+
+public class SensorHandler {
+    
+    private final RobotController myRC;
+    private final Knowledge       knowledge;
+    
+    
+    /*** Controller ***/
+    private final SensorController[] mySensorSlots
+    private final SensorController[] mySCs
+    private final int                numberOfSensors;
+
+    public SensorHandler(RobotController rc, Knowledge know) {
+        
+        myRC      = rc;
+        knowledge = know;
+    }
+    
+}
