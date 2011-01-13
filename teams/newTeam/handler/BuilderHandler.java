ce62c5f698d56fc3e69a36d0a825259f8cff3e06
jrshoch <jrshoch@gmail.com>
2011-01-13 16:28:45 -0500

    BuilderHandler added

---
+++ teams/newTeam/handler/BuilderHandler.java
@@ -0,0 +1,13 @@
+package newTeam.handler;
+
+import battlecode.common.*;
+
+public class BuilderHandler {
+    
+    private BuilderController myBC;
+    
+    public void addBC(BuilderController bc) {
+        myBC = bc;
+    }
+
+}
