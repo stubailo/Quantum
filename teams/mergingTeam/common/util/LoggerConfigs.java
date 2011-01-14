ca6cd8b3d6098c9e6a834aaf422753913f3b7021
unknown <sashko@.(none)>
2011-01-14 04:10:24 -0500

    finally merged my stuff. phew.

--- teams/mergingTeam/common/util/LoggerConfigs.java
+++
@@ -1,9 +0,0 @@
-package newTeam.common.util;
-
-public class LoggerConfigs {
-    
-    public static final boolean SASHKO_STRINGS = true;
-    public static final boolean HOCHO_STRINGS  = true;
-    public static final boolean ANTONY_STRINGS = true;
-
-}
